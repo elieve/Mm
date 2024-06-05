c953ba97237c598c972d78aef68ab1807c4813ea
Lucifer <38715155+jonesroot@users.noreply.github.com>
2024-06-05 17:59:54 +0700

    update modular/calc.py

--- modular/calc.py
+++ modular/calc.py
@@ -48,12 +48,18 @@ CALCULATE_BUTTONS = InlineKeyboardMarkup(
 
 CALCULATE_TEXT = "Mix-Userbot Calculator"
 
-
 @ky.ubot("calc|kalku", sudo=True)
 async def _(c: nlx, m):
+    try:
+        x = await c.get_inline_bot_results(bot.me.username, "calc")
+        await m.reply_inline_bot_result(x.query_id, x.results[0].id)
+    except Exception as error:
+        await m.reply(error)
+    '''
     await m.reply_text(
         text=CALCULATE_TEXT,
         reply_markup=CALCULATE_BUTTONS,
         disable_web_page_preview=True,
         quote=True,
     )
+    '''
