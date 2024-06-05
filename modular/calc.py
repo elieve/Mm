d2005de52a0fd299679428ee4fca1156f25ea086
Lucifer <38715155+jonesroot@users.noreply.github.com>
2024-06-05 17:48:45 +0700

    update modular/calc.py

--- modular/calc.py
+++ modular/calc.py
@@ -6,6 +6,47 @@ from Mix import *
 __modles__ = "Calculator"
 __help__ = "Calculator"
 
+CALCULATE_BUTTONS = InlineKeyboardMarkup(
+    [
+        [
+            InlineKeyboardButton("(", callback_data="("),
+            InlineKeyboardButton(")", callback_data=")"),
+            InlineKeyboardButton("^", callback_data="^"),
+        ],
+        [
+            InlineKeyboardButton("%", callback_data="%"),
+            InlineKeyboardButton("AC", callback_data="AC"),
+            InlineKeyboardButton("DEL", callback_data="DEL"),
+            InlineKeyboardButton("÷", callback_data="/"),
+        ],
+        [
+            InlineKeyboardButton("7", callback_data="7"),
+            InlineKeyboardButton("8", callback_data="8"),
+            InlineKeyboardButton("9", callback_data="9"),
+            InlineKeyboardButton("×", callback_data="*"),
+        ],
+        [
+            InlineKeyboardButton("4", callback_data="4"),
+            InlineKeyboardButton("5", callback_data="5"),
+            InlineKeyboardButton("6", callback_data="6"),
+            InlineKeyboardButton("-", callback_data="-"),
+        ],
+        [
+            InlineKeyboardButton("1", callback_data="1"),
+            InlineKeyboardButton("2", callback_data="2"),
+            InlineKeyboardButton("3", callback_data="3"),
+            InlineKeyboardButton("+", callback_data="+"),
+        ],
+        [
+            InlineKeyboardButton("00", callback_data="00"),
+            InlineKeyboardButton("0", callback_data="0"),
+            InlineKeyboardButton("=", callback_data="="),
+            InlineKeyboardButton(".", callback_data="."),
+        ],
+    ]
+)
+
+CALCULATE_TEXT = "Mix-Userbot Calculator"
 
 @ky.ubot("calc|kalku", sudo=True)
 async def _(c: nlx, m):
