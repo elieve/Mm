6212642a214765750900ffbce067aa0d21c7c9b4
jonesroot <jonesroot@users.noreply.github.com>
2024-06-05 11:09:13 +0000

    Auto Fix

--- assistant/inline.py
+++ assistant/inline.py
@@ -489,44 +489,44 @@ async def _(c, iq):
 @ky.inline("^calc")
 async def inline(bot, update):
     CALCULATE_BUTTONS = InlineKeyboardMarkup(
-      [
-          [
-              InlineKeyboardButton("(", callback_data="("),
-              InlineKeyboardButton(")", callback_data=")"),
-              InlineKeyboardButton("^", callback_data="^"),
-          ],
-          [
-              InlineKeyboardButton("%", callback_data="%"),
-              InlineKeyboardButton("AC", callback_data="AC"),
-              InlineKeyboardButton("DEL", callback_data="DEL"),
-              InlineKeyboardButton("÷", callback_data="/"),
-          ],
-          [
-              InlineKeyboardButton("7", callback_data="7"),
-              InlineKeyboardButton("8", callback_data="8"),
-              InlineKeyboardButton("9", callback_data="9"),
-              InlineKeyboardButton("×", callback_data="*"),
-          ],
-          [
-              InlineKeyboardButton("4", callback_data="4"),
-              InlineKeyboardButton("5", callback_data="5"),
-              InlineKeyboardButton("6", callback_data="6"),
-              InlineKeyboardButton("-", callback_data="-"),
-          ],
-          [
-              InlineKeyboardButton("1", callback_data="1"),
-              InlineKeyboardButton("2", callback_data="2"),
-              InlineKeyboardButton("3", callback_data="3"),
-              InlineKeyboardButton("+", callback_data="+"),
-          ],
-          [
-              InlineKeyboardButton("00", callback_data="00"),
-              InlineKeyboardButton("0", callback_data="0"),
-              InlineKeyboardButton("=", callback_data="="),
-              InlineKeyboardButton(".", callback_data="."),
-          ],
-      ]
-  )
+        [
+            [
+                InlineKeyboardButton("(", callback_data="("),
+                InlineKeyboardButton(")", callback_data=")"),
+                InlineKeyboardButton("^", callback_data="^"),
+            ],
+            [
+                InlineKeyboardButton("%", callback_data="%"),
+                InlineKeyboardButton("AC", callback_data="AC"),
+                InlineKeyboardButton("DEL", callback_data="DEL"),
+                InlineKeyboardButton("÷", callback_data="/"),
+            ],
+            [
+                InlineKeyboardButton("7", callback_data="7"),
+                InlineKeyboardButton("8", callback_data="8"),
+                InlineKeyboardButton("9", callback_data="9"),
+                InlineKeyboardButton("×", callback_data="*"),
+            ],
+            [
+                InlineKeyboardButton("4", callback_data="4"),
+                InlineKeyboardButton("5", callback_data="5"),
+                InlineKeyboardButton("6", callback_data="6"),
+                InlineKeyboardButton("-", callback_data="-"),
+            ],
+            [
+                InlineKeyboardButton("1", callback_data="1"),
+                InlineKeyboardButton("2", callback_data="2"),
+                InlineKeyboardButton("3", callback_data="3"),
+                InlineKeyboardButton("+", callback_data="+"),
+            ],
+            [
+                InlineKeyboardButton("00", callback_data="00"),
+                InlineKeyboardButton("0", callback_data="0"),
+                InlineKeyboardButton("=", callback_data="="),
+                InlineKeyboardButton(".", callback_data="."),
+            ],
+        ]
+    )
     CALCULATE_TEXT = "Mix-Userbot Calculator"
     if len(update.data) == 0:
         try:
