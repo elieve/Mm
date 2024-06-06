9dbccf3aff962bfba29f07e28562e1b4c07dbe38
Lucifer <38715155+jonesroot@users.noreply.github.com>
2024-06-06 14:00:57 +0700

    update modular/calc.py

--- modular/calc.py
+++ modular/calc.py
@@ -94,7 +94,7 @@ async def _(c: nlx, m):
         await m.reply_text(str(error))
 
 
-@ky.callback("^.*")
+@ky.callback("^[0-9/*\-+().=ACDEL%]{1}$")
 async def _(c, cq):
     global hitung
     data = cq.data
