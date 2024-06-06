4016bc9d768b81201c7966718a351ce561ea2ae6
Lucifer <38715155+jonesroot@users.noreply.github.com>
2024-06-06 15:26:47 +0700

    update assistant/call_calc.py

--- assistant/call_calc.py
+++ assistant/call_calc.py
@@ -35,45 +35,48 @@ mmk = {
 
 
 def get_calculator_buttons():
-    return ReplyKeyboardMarkup(
+    return InlineKeyboardMarkup(
         [
             [
                 InlineKeyboardButton("(", callback_data="("),
                 InlineKeyboardButton(")", callback_data=")"),
-                InlineKeyboardButton("❌", callback_data="KLOS"),
+                InlineKeyboardButton("%", callback_data="%"),
+                InlineKeyboardButton("⌫", callback_data="DEL"),
             ],
             [
-                InlineKeyboardButton("🆑", callback_data="AC"),
-                InlineKeyboardButton("⌫", callback_data="DEL"),
-                InlineKeyboardButton("%", callback_data="%"),
+                InlineKeyboardButton("✖️", callback_data="*"),
+                InlineKeyboardButton("➕", callback_data="+"),
+                InlineKeyboardButton("➖", callback_data="-"),
                 InlineKeyboardButton("➗", callback_data="/"),
             ],
             [
                 InlineKeyboardButton("7️⃣", callback_data="7"),
                 InlineKeyboardButton("8️⃣", callback_data="8"),
                 InlineKeyboardButton("9️⃣", callback_data="9"),
-                InlineKeyboardButton("✖️", callback_data="*"),
             ],
             [
                 InlineKeyboardButton("4️⃣", callback_data="4"),
                 InlineKeyboardButton("5️⃣", callback_data="5"),
                 InlineKeyboardButton("6️⃣", callback_data="6"),
-                InlineKeyboardButton("➖", callback_data="-"),
             ],
             [
                 InlineKeyboardButton("1️⃣", callback_data="1"),
                 InlineKeyboardButton("2️⃣", callback_data="2"),
                 InlineKeyboardButton("3️⃣", callback_data="3"),
-                InlineKeyboardButton("➕", callback_data="+"),
             ],
             [
                 InlineKeyboardButton("0️⃣0️⃣", callback_data="00"),
                 InlineKeyboardButton("0️⃣", callback_data="0"),
                 InlineKeyboardButton("◾", callback_data="."),
-                InlineKeyboardButton("🟰", callback_data="="),
             ],
-        ],
-        resize_keyboard=True,
+            [
+                InlineKeyboardButton("🟰", callback_data="="),
+                InlineKeyboardButton("🆑", callback_data="AC"),
+            ],
+            [
+                InlineKeyboardButton("❌", callback_data="KLOS"),
+            ],
+        ]
     )
 
 
