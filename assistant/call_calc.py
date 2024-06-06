6979b364644754f4b77cf7318c7f9de6ba87f54b
Lucifer <38715155+jonesroot@users.noreply.github.com>
2024-06-06 15:59:28 +0700

    update assistant/call_calc.py

--- assistant/call_calc.py
+++ assistant/call_calc.py
@@ -1,3 +1,5 @@
+from attrify import Attrify as Atr
+
 from pyrogram import *
 from pyrogram.enums import *
 from pyrogram.types import *
@@ -146,3 +148,20 @@ Mix-Userbot Calculator
             await cq.answer(f"Error: {str(e)}", show_alert=True)
     else:
         await cq.answer(f"{current_text}")
+
+
+def unpacked2(inline_message_id: str):
+    dc_id, message_id, chat_id, query_id = unpack(
+        "<iiiq",
+        urlsafe_b64decode(
+            inline_message_id + "=" * (len(inline_message_id) % 4),
+        ),
+    )
+    temp = {
+        "dc_id": dc_id,
+        "message_id": message_id,
+        "chat_id": int(str(chat_id).replace("-", "-100")),
+        "query_id": query_id,
+        "inline_message_id": inline_message_id,
+    }
+    return Atr(temp)
