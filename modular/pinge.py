e6f36aaa3a3d8fc689907b163a976ca96c7b2705
Lucifer <38715155+jonesroot@users.noreply.github.com>
2024-06-09 22:36:33 +0700

    update modular/pinge.py

--- modular/pinge.py
+++ modular/pinge.py
@@ -25,9 +25,10 @@ async def _(c: nlx, m):
     start = datetime.now()
     await c.invoke(Ping(ping_id=0))
     end = datetime.now()
-    delta_ping = (end - start).microseconds / 10000
+    delta_ping = (end - start).microseconds / 1000
+    emping = delta_ping / 10
     upnya = await get_time((time() - start_time))
     _ping = cgr("ping_1").format(
-        em.ping, str(delta_ping).replace(".", ","), em.pong, upnya
+        em.ping, str(emping).replace(".", ","), em.pong, upnya
     )
     await m.reply(_ping)
