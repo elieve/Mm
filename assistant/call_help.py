0536e7471828a413e6782f9db65770895a6559aa
naya1503 <118755946+naya1503@users.noreply.github.com>
2024-06-25 16:32:46 +0700

    update assistant/call_help.py

--- assistant/call_help.py
+++ assistant/call_help.py
@@ -21,7 +21,7 @@ from pyrogram.enums import *
 from pyrogram.errors import *
 from pyrogram.raw.functions.messages import DeleteHistory
 from pyrogram.types import *
-
+from pyrogram.helpers import ikb
 from Mix import *
 from Mix.core.waktu import get_time, start_time
 from modular.pmpermit import *
