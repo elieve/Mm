b00249175fb315e450c2be1781b485d76759d089
naya1503 <118755946+naya1503@users.noreply.github.com>
2024-06-25 16:01:35 +0700

    update modular/alive.py

--- modular/alive.py
+++ modular/alive.py
@@ -15,6 +15,15 @@ __help__ = get_cgr("help_alive")
 
 @ky.ubot("alive", sudo=True)
 async def _(c: nlx, m):
+    if not udB.get_var(c.me.id, "gc"):
+        gc = await c.get_chats_dialog(c, "grup")
+        udB.set_var(c.me.id, "gc", len(gc))
+    elif not udB.get_var(c.me.id, "us"):
+        us = await c.get_chats_dialog(c, "user")
+        udB.set_var(c.me.id, "us", len(us))
+    elif not udB.get_var(c.me.id, "bot"):
+        bt = await c.get_chats_dialog(c, "bot")
+        udB.set_var(c.me.id, "bot", len(bt))
     try:
         x = await c.get_inline_bot_results(bot.me.username, "alive")
         await m.reply_inline_bot_result(x.query_id, x.results[0].id)
