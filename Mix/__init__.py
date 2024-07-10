fb0ffecd96f51de77117201d5d683142385f365d
naya1503 <118755946+naya1503@users.noreply.github.com>
2024-07-10 17:07:11 +0700

    update Mix/__init__.py

--- Mix/__init__.py
+++ Mix/__init__.py
@@ -14,7 +14,14 @@ bot = Bot()
 nlx = Userbot()
 
 
-from team import *
+from team.nandev.class_log import LOGGER
+from team.nandev.database import ndB, udB
+from team.nandev.autopilot import autobot
+from team.nandev.class_handler import ky, human, TAG_LOG, refresh_cache, isFinish
+from team.nandev.class_modules import CMD_HELP, paginate_modules
+from team.nandev.class_emoji import Emojik
+from team.nandev.class_pytgc import unpackInlineMessage, run_sync, YoutubeDownload, YouTubeSearch
+from team.nandev.new_database import Greetings, Users, LOCKS, Approve, Notes, NotesSettings, Filters, GBan, GMute
 from thegokil import DEVS, NO_GCAST
 
 from langs import *
