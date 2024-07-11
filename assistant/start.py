edcf94624ee39ade5a4fcbf4ed44aac8514aa743
elieve <bayustiadi@ymail.com>
2024-07-11 23:49:39 +0800

    Update start.py

--- assistant/start.py
+++ assistant/start.py
@@ -19,28 +19,6 @@ from pyrogram.types import *
 
 from Mix import *
 
-help =
-async def _(c: nlx, m):
-    em = Emojik()
-    em.initialize()
-    if not c.get_arg(m):
-        try:
-            x = await c.get_inline_bot_results(bot.me.username, "help")
-            await m.reply_inline_bot_result(x.query_id, x.results[0].id)
-        except Exception as error:
-            await m.reply(error)
-    else:
-        nama = c.get_arg(m)
-        if c.get_arg(m) in CMD_HELP:
-            prefix = await c.get_prefix(c.me.id)
-            await m.reply(
-                CMD_HELP[c.get_arg(m)].__help__.format(next((p) for p in prefix))
-                + f"\n<b>© Max-Userbot - @ccavolo</b>",
-                quote=True,
-            )
-        else:
-            await m.reply(cgr("hlp_1").format(em.gagal, nama))
-
 
 
 @ky.bots("start", human.pv)
@@ -56,7 +34,7 @@ async def _(c, m):
             [
                 [(cgr("asst_3"), "clbk.bhsa"), (cgr("asst_6"), "clbk.rebot")],
                 [("Repo", "https://github.com/elieve/mm", "url"), ("Buat Bot", "t.me/ccavolo", "url")],
-                [(cgr("ttup"), "help")],
+                [(cgr("ttup"), "module")],
             ]
         )
         return await m.reply(ts_1, reply_markup=button)
