fabda1a39c436d78c8cc8d9398f15eac26c5532e
elieve <bayustiadi@ymail.com>
2024-07-11 22:34:04 +0800

    Update start.py

--- assistant/start.py
+++ assistant/start.py
@@ -30,10 +30,10 @@ async def _(c, m):
     ts_2 = cgr("asst_2").format(user_name, user2)
     if m.from_user.id == owner_nih:
         button = ikb(
-            [
+            
                 [(cgr("asst_3"), "clbk.bhsa"), (cgr("asst_6"), "clbk.rebot")],
-                [(cgr("ttup"), "close_asst"), (cgr("Buat Bot"), "@ccavolo")],
-                [(cgr("Repo"), "https://github.com/elieve/mm")],
+                [("Repo", "https://github.com/elieve/mm", "url"), ("Buat Bot"), "t.me/ccavolo", "url")],
+                [(cgr("ttup"), "close_asst")],
             ]
         )
         return await m.reply(ts_1, reply_markup=button)
